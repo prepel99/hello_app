304676a56615388269da9d3924ff95e7ef51757d
user <prepel99@gmail.com>
2018-01-18 03:48:24 +0200

    Changed application_controller.rb and routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-	def hello
-		render html: "hello, world!!!"
+	def bye
+		render html: "goodbye, world!!!(((("
 	end
 end
